c95eea883abebef0e737230870866ab8701650d7
Sezahija Muslic <sezahijamuslic@rezolve.com>
2023-10-18 17:01:28 +0200

    another commit

---
+++ basics4.js
@@ -0,0 +1,15 @@
+function add(a,b)
+{
+    return a+b
+}
+
+let sum= add(2,3)
+console.log(sum)
+
+let day = 'tuesday '
+console.log(day.length)
+let subday = day.slice(0,4)
+console.log(subday)
+console.log(day[1])
+let splitDay = day.split('u')
+console.log(splitDay)
